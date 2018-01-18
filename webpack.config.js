d072ac16919de019d8e6194518efd56230c9fd9f
Dániel Kántor <git@daniel-kantor.com>
2018-01-18 18:51:54 +0100

    Added skeleton

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [new HtmlWebpackPlugin()]
+};
