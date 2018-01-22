c50d282900999fc3931b22b49bd1a1a409d6f51b
Daniel Kantor <github@daniel-kantor.com>
2018-01-22 16:18:12 +0000

    Implement base site (#1)
    * Added skeleton
    
    * .
    
    * added CNAME
    
    * .
    
    * .
    
    * .
    
    * added viewport tag
    
    * Removed grid

---
+++ index.js
@@ -0,0 +1,7 @@
+import default_parser from 'kantord/dashboard'
+
+new Navigo();
+
+
+
+console.log(dashboard())
---
+++ src/index.js
@@ -0,0 +1,21 @@
+import dashboard from 'dashboard'
+import '../dist/fonts.css'
+import * as d3 from 'd3'
+
+const render_dashboard = (data) =>
+    dashboard(data)(d3.selection())
+
+const get_gist_id = () =>
+    window.location.pathname.match(/\/e\/[^\/]+\/(.+)/)[1]
+
+
+const load_gist = (id) =>
+    d3.json(`https://api.github.com/gists/${id}`, (data) => {
+       const url = data.files["dashboard.json"].raw_url
+        d3.json(url, render_dashboard)
+    })
+
+
+document.addEventListener("DOMContentLoaded", function(event) { 
+    load_gist(get_gist_id())
+});
