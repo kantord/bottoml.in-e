48ccf9cc7e15eb9a3636005c28a46b1dab9b7486
Dániel Kántor <git@daniel-kantor.com>
2018-01-25 17:13:38 +0100

    Update

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,8 @@ module.exports = {
   },
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
   },
   devServer: {
     contentBase: './dist'
