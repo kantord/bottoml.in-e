c116266b0a5ce81b41df35ddaa712839b839266f
Dániel Kántor <git@daniel-kantor.com>
2018-01-29 17:22:08 +0100

    new version

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
-import dashboard from 'just-dashboard'
+import { json_parser, yaml_parser } from 'just-dashboard'
 import * as d3 from 'd3'
 
 const render_dashboard = (data) =>
-    dashboard(data)(d3.selection())
+    json_parser(data)(d3.selection())
+
+const render_dashboard_yaml = (data) => {
+  render_dashboard(yaml_parser(data))
+}
 
 const get_gist_id = () =>
     window.location.pathname.match(/\/e\/[^\/]+\/(.+)/)[1]
@@ -10,8 +14,15 @@ const get_gist_id = () =>
 
 const load_gist = (id) =>
     d3.json(`https://api.github.com/gists/${id}`, (data) => {
+      if (data.files['dashboard.json'] !== undefined) {
        const url = data.files["dashboard.json"].raw_url
         d3.json(url, render_dashboard)
+      } else {
+        if (data.files['dashboard.yml'] !== undefined) {
+       const url = data.files["dashboard.yml"].raw_url
+        d3.text(url, render_dashboard_yaml)
+      }
+      }
     })
 
 
